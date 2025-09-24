c5c7b7a9850067cedf29461d2aa7d596e5db3ecf
Nilay Namit <109901909+LuckyNilay15@users.noreply.github.com>
2025-09-24 12:16:30 +0530

    Update Tag.ts

--- src/app/_models/Tag.ts
+++ src/app/_models/Tag.ts
@@ -2,7 +2,9 @@ export class Tag {
     static readonly PYTHON = new Tag('Python', 'teal');
     static readonly ANGULAR = new Tag('Angular', 'blue');
     static readonly TYPESCRIPT = new Tag('TypeScript', 'lightblue');
-    static readonly JULIA = new Tag('Julia', 'green');
+    static readonly Node = new Tag('Node', 'green');
+    static readonly Express = new Tag('Express', 'blue');
+    static readonly MongoDB = new Tag('MongoDB', 'purple');
     static readonly LLM = new Tag('LLM', 'lightgreen');
     static readonly REACT = new Tag('React', 'purple');
     private constructor(private readonly key:string, public readonly color:string) {}
